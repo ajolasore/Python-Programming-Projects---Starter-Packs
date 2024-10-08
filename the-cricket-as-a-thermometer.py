ff6a58ad2691e87c5a53a29624e3584dfa2020fb
ajolasore <118432125+ajolasore@users.noreply.github.com>
2024-10-08 17:49:16 +0200

    Delete the-cricket-as-a-thermometer.py

--- the-cricket-as-a-thermometer.py
+++
@@ -1,7 +0,0 @@
-N60 = int(input("the number of observed chirps per minute:"))
-
-TF = 50 + ((N60 - 40)/4)
-TC = 10 + ((N60 - 40)/7)
-
-print("temperature (Fahrenheit):", TF)
-print("temperature (Celsius):", TC)
